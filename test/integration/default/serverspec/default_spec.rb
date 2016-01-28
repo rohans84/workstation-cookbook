d7f73e4a1fd96d023b7f8af8b0c199fbd339fbfc
John Doe <jdoe@example>
2016-01-28 21:37:09 +0000

    added more tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -7,4 +7,8 @@ describe 'workstation::default' do
     describe package('tree') do
         it {should be_installed} 
     end
+
+    describe file('/etc/motd') do
+	it {should be_owned_by 'root'}
+    end
 end
